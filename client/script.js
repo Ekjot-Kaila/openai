ef666e736ba812ce9241083fa8e15a2138406b7f
“EkjotKaila” <ekaila1@my.bcit.ca>
2023-05-11 18:41:35 -0700

    minor change

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ chatContainer.scrollTop = chatContainer.scrollHeight;
 const messageDiv = document.getElementById(uniqueID);
 loader(messageDiv);
 // fetch data from server -> bot's response
-const response = await fetch('http://localhost:5000', {
+const response = await fetch('https://openai-wb2i.onrender.com5000', {
 method: 'POST',
 headers:{
   'Content-Type': 'application/json'
