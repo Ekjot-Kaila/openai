52daf79ae06904d2a1ede5d7aca8f9573cf633bf
“EkjotKaila” <ekaila1@my.bcit.ca>
2023-05-16 12:09:30 -0700

    add conversation history

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from "openai";
 dotenv.config();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
@@ -14,38 +14,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/' , async(req,res)=>{
-res.status(200).send({
- message: 'Hello from Codex',   
+// Conversation history storage
+let conversation = [];
 
-})
-})
+app.get('/', async (req, res) => {
+  res.status(200).send({
+    message: 'Hello from Codex',
+  });
+});
 
-app.post('/', async(req, res)=>{
-try{
+app.post('/', async (req, res) => {
+  try {
     const prompt = req.body.prompt;
     const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: `Pretend you are a personal tutor. Answer with a positive tone. 
-        
-        ${prompt}`,
-        temperature: 0.5,
-        max_tokens: 3000,
-        top_p: 1.0,
-        frequency_penalty: 0.5,
-        presence_penalty: 0.0,
-        
+      model: "text-davinci-003",
+      prompt: `${conversation} ${prompt}`,
+      temperature: 0.5,
+      max_tokens: 3000,
+      top_p: 1.0,
+      frequency_penalty: 0.5,
+      presence_penalty: 0.0,
     });
+
+    const botResponse = response.data.choices[0].text;
+
+    // Add user input and AI response to conversation history
+    conversation.push({ role: 'user', content: prompt });
+    conversation.push({ role: 'bot', content: botResponse });
+
     res.status(200).send({
-        bot:response.data.choices[0].text
-    
-    })
-}catch (error){
-console.log(error);
-res.status(500).send({error})
-}
+      bot: botResponse,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
+});
 
-
-})
-
-app.listen(5000,()=> console.log('server is running on port http://localhost:5000'));
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
