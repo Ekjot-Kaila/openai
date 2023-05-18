1d7d4b33e43fc22c51ea72744baed0e4b78c976f
“EkjotKaila” <ekaila1@my.bcit.ca>
2023-05-18 09:16:49 -0700

    localhost:5000

--- client/script.js
+++ client/script.js
@@ -70,7 +70,7 @@ chatContainer.scrollTop = chatContainer.scrollHeight;
 const messageDiv = document.getElementById(uniqueID);
 loader(messageDiv);
 // fetch data from server -> bot's response
-const response = await fetch('https://serversideopenai.onrender.com', {
+const response = await fetch('http://localhost:5000', {
 method: 'POST',
 headers:{
   'Content-Type': 'application/json'
